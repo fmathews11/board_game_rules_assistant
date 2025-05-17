76043c4248a6f6586ac38037e2601ec4d04ed828
fmathews11 <fmathews@gmail.com>
2025-05-16 22:04:59 -0400

    Refactor PDF text extraction and update file references.

--- Extract_Raw_Text_From_PDF_Files.py
+++ Extract_Raw_Text_From_PDF_Files.py
@@ -5,15 +5,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 SYSTEM_PROMPT = """You are a helpful assistant.  
-You are proficient in extracting text from images using OCR.  
-The text you extract is returned in a markdown format with a header at the beginning of each page.  
+You are proficient in extracting text from PDFS, using OCR when necessary.  
+The text you extract must be returned in a markdown format with a header at the beginning of each page.  
 The header includes the page number.  Format your response for optimal use for a
  language model RAG question/answer application.
  """
-FILE_PATH = "raw_pdf_files/wingspan.pdf"
+FILE_PATH = "raw_pdf_files/scythe.pdf"
+MODEL = "gemini-2.5-flash-preview-04-17"
 
 
-def generate():
+def extract():
     client = genai.Client(
         api_key=os.environ.get("GEMINI_API_KEY"),
     )
@@ -21,7 +22,7 @@ def generate():
     files = [
         client.files.upload(file=FILE_PATH),
     ]
-    model = "gemini-2.5-flash-preview-04-17"
+    model = MODEL
     contents = [
         types.Content(
             role="user",
@@ -30,12 +31,12 @@ def generate():
                     file_uri=files[0].uri,
                     mime_type=files[0].mime_type,
                 ),
-                types.Part.from_text(text="""INSERT_INPUT_HERE"""),
+                types.Part.from_text(text="Extract text from this PDF"),
             ],
         ),
     ]
     generate_content_config = types.GenerateContentConfig(
-        temperature=0.2,
+        temperature=0,
         response_mime_type="text/plain",
         system_instruction=[
             types.Part.from_text(text=SYSTEM_PROMPT),
@@ -48,11 +49,11 @@ def generate():
         config=generate_content_config,
     )
 
-    output_file_path = "text/wingspan.txt"
+    output_file_path = "text/scythe.txt"
     with open(output_file_path, "w", encoding="utf-8") as f:
         f.write(response.text)
     print(f"Extracted text saved to {output_file_path}")
 
 
 if __name__ == "__main__":
-    generate()
+    extract()
