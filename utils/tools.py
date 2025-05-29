cb07ffea323332adad588fcd3c7fa5e6d8f86943
fmathews11 <fmathews@gmail.com>
2025-05-29 15:31:05 -0400

    Implemented standalone question from chat history methodology

--- utils/tools.py
+++ utils/tools.py
@@ -1,7 +1,9 @@
+from operator import itemgetter
+from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_tavily import TavilySearch
-from utils.prompts import SPIRIT_ISLAND_SEARCH_PROMPT
+from utils.prompts import SPIRIT_ISLAND_SEARCH_PROMPT, STANDALONE_QUESTION_PROMPT
 import os
 import dotenv
 
@@ -13,6 +15,22 @@ tavily_search_object = TavilySearch(max_results=10,
                                     include_domains=['https://spiritislandwiki.com/'])
 
 
+def _extract_raw_text_from_message_history(message_list: list) -> str:
+    """
+    Extracts raw text from a history of message objects.
+
+    :param message_list: A list of message objects, consisting of instances of
+        HumanMessage and AIMessage, whose text content will be extracted and
+        formatted into a single raw text block.
+    :return: A string containing the formatted raw text of the message history,
+        where each message is prefixed by its sender type ('Human:' or 'AI:')
+    """
+    return "\n".join([f"Human: {message.content}"
+                      if isinstance(message, HumanMessage)
+                      else f"AI: {message.content}" for message
+                      in message_list if isinstance(message, (HumanMessage, AIMessage))])
+
+
 def _get_tavily_search_spirit_island_text(question: str) -> str:
     """
     Retrieves and processes the search results for the specified question using
@@ -21,7 +39,9 @@ def _get_tavily_search_spirit_island_text(question: str) -> str:
     :param question: The search query to be processed.
     """
     response = tavily_search_object.invoke(question)
-    return " ".join([i['raw_content'] or "" for i in response['results']])
+    return " ".join([i['raw_content'] + f"Found in {i['url']}"
+                     for i in response['results']
+                     if i.get('raw_content')])
 
 
 def create_spirit_island_search_chain(llm):
@@ -42,3 +62,19 @@ def create_spirit_island_search_chain(llm):
             | StrOutputParser())
 
     return chain
+
+
+def create_standalone_question_from_chat_history(chat_history: list,
+                                                 users_question: str,
+                                                 llm) -> str:
+
+    formatted_chat_history = _extract_raw_text_from_message_history(chat_history)
+    chain = (
+            {"chat_history": itemgetter("chat_history"), "user_question": itemgetter("user_question")}
+            | STANDALONE_QUESTION_PROMPT
+            | llm
+            | StrOutputParser()
+    )
+    return chain.invoke({'chat_history': formatted_chat_history, 'user_question': users_question})
+
+
