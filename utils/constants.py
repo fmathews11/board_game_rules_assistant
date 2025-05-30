ab713fb199d3f359be701e552f4cf8413d2e8f5f
fmathews11 <fmathews@gmail.com>
2025-05-30 16:26:41 -0400

    Add constants for Tavily search markers in board game rules assistant

---
+++ utils/constants.py
@@ -0,0 +1,7 @@
+"""
+Constants used throughout the board game rules assistant.
+"""
+
+# Key phrases for using Tavily search as a tool
+TAVILY_SEARCH_MARKER = "[TAVILY_SEARCH_RECOMMENDED_FOR_SPIRIT_ISLAND]"
+INSUFFICIENT_SEARCH_RESULTS_MARKER = "[INSUFFICIENT_SEARCH_RESULTS]"
